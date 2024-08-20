e4f11e9179dcc8ef611e1dfc0ce36841e30a11bc
kemal.ozkesen <kemalozkesen@gmail.com>
2024-08-20 16:09:53 +0300

    Seed Data Added

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Data.Entity;
+using System.Reflection;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<ProductFeature> ProductFeature { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Apply all configurations
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            //Seed data from model creating
+            modelBuilder.Entity<ProductFeature>().HasData(new ProductFeature()
+            {
+                Id = 1,
+                Color = "Red",
+                Heigth = 100,
+                Weigth = 200,
+                ProductId = 1
+            },
+            new ProductFeature()
+            {
+                Id = 2,
+                Color = "Blue",
+                Heigth = 100,
+                Weigth = 200,
+                ProductId = 2
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ NLayer.Repository/Seed/ProductSeed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Data.Entity;
+
+namespace NLayer.Repository.Seed
+{
+    internal class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product() { Id = 1, CategoryId = 1, Name = "Kalem 1", Price = 100, Stock = 50, InsertDate = DateTime.UtcNow },
+                new Product() { Id = 2, CategoryId = 2, Name = "Kitap 1", Price = 200, Stock = 150, InsertDate = DateTime.UtcNow },
+                new Product() { Id = 3, CategoryId = 2, Name = "Defter 1", Price = 300, Stock = 250, InsertDate = DateTime.UtcNow });
+        }
+    }
+}
