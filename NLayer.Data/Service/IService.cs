4f12025cf15487cce517e59891522229e2e09886
kemal.ozkesen <kemalozkesen@gmail.com>
2024-08-22 14:11:09 +0300

    Generic Service class was writen

--- NLayer.Data/Service/IService.cs
+++ NLayer.Data/Service/IService.cs
@@ -5,12 +5,12 @@ namespace NLayer.Data.Service
     public interface IService<T> where T : class
     {
         Task<T> GetByIdAsync(int id);
-        Task<IQueryable<T>> GetAllAsync();
-        Task AddAsync(T entity);
-        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
         Task UpdateAsync(T entity);
         Task DeleteAsync(T entity);
-        Task DeleteRange(IEnumerable<T> entities);
+        Task DeleteRangeAsync(IEnumerable<T> entities);
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
     }
---
+++ NLayer.Service/Services/GenericService.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Data.Repository;
+using NLayer.Data.Service;
+using NLayer.Data.UnitOfWork;
+using NLayer.Service.Exception;
+using System.Linq.Expressions;
+
+namespace NLayer.Service.Services
+{
+    public class GenericService<T> : IService<T> where T : class
+    {
+        private readonly IGenericRepository<T> _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GenericService(IGenericRepository<T> repository, IUnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _repository.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _repository.AddRangeAsync(entities);
+            await _unitOfWork.CommitAsync();
+            return entities;
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _repository.AnyAsync(expression);
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _repository.Delete(entity);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task DeleteRangeAsync(IEnumerable<T> entities)
+        {
+            _repository.DeleteRange(entities);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _repository.GetAll().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var entity = await _repository.GetByIdAsync(id);
+
+            if (entity == null)
+            {
+                throw new NotFoundException($"{typeof(T).Name} not found. Id: {id}");
+            }
+
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _repository.Update(entity);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            return _repository.Where(expression);
+        }
+    }
+}
