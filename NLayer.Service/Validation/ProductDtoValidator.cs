7fe95607d6b3379eb92bf482229820bcb53c3e5e
kemal.ozkesen <kemalozkesen@gmail.com>
2024-08-22 16:04:32 +0300

    Fluent validator class was implemented for dtos

---
+++ NLayer.Service/Validation/ProductDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using NLayer.Data.Dto;
+
+namespace NLayer.Service.Validation
+{
+    public class ProductDtoValidator : AbstractValidator<ProductDto>
+    {
+        public ProductDtoValidator()
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
+            RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
+            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater than 0");
+        }
+    }
+}
