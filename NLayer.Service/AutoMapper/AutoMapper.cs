d39c637bb06841bdea8771587c993b8ebd0a1c8e
kemal.ozkesen <kemalozkesen@gmail.com>
2024-08-22 13:44:02 +0300

    Auto Mapper Profile added

---
+++ NLayer.Service/AutoMapper/AutoMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using NLayer.Data.Dto;
+using NLayer.Data.Entity;
+
+namespace NLayer.Service.AutoMapper
+{
+    public class AutoMapper : Profile
+    {
+        public AutoMapper()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
+            CreateMap<ProductUpdateDto, Product>();
+            CreateMap<Product, ProductWithCategoryDto>();
+            CreateMap<Product, CategoryWithProductDto>();
+        }
+    }
+}
