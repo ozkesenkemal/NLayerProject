2e705c013122f6866c95379ca49bcf94b20560b3
kemal.ozkesen <kemalozkesen@gmail.com>
2024-08-23 14:04:17 +0300

    Base Controller added

---
+++ NLayer.API/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.Data.Dto;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        private const int NoContentResponse = 204;
+
+        [NonAction]
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> response)
+        {
+            if(response.StatusCode == NoContentResponse) 
+            {
+                return new ObjectResult(null) { StatusCode = response.StatusCode };
+            }
+
+            return new ObjectResult(response) { StatusCode = response.StatusCode };
+        }
+    }
+}
